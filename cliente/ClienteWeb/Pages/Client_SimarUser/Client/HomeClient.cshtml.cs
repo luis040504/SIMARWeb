6dd1599da1b30be58968232a1f6feaa64e302eb3
Ness-luna <moralesvanessa2010@gmail.com>
2026-03-19 20:33:37 -0600

    Interfaces: crud Simar Users

---
+++ cliente/ClienteWeb/Pages/Client_SimarUser/Client/HomeClient.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClienteWeb.Pages.Client_SimarUser.Client
+{
+    public class HomeClienteModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
+
