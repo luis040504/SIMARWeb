ed4ce5d011af4f4ed0db3c5873e8b577ad7c9310
Yael <yaelalfredosalazaraguilar14@gmail.com>
2026-04-10 15:44:20 -0600

    feat: setup contracts microservice with EF Core, Docker and SQL Server integration

--- servidor/servicios/API_Contracts/ContractsService/Program.cs
+++ servidor/servicios/API_Contracts/ContractsService/Program.cs
@@ -1,12 +1,15 @@
+using ContractsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<ContractsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
